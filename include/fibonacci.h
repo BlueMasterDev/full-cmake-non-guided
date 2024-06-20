d8eeac32f3861fb37c7d1d6442403a78e82d2f8d
BlueMaster <blueshark936@gmail.com>
2024-06-20 14:19:01 +0200

    Implement all headers files

---
+++ include/fibonacci.h
@@ -0,0 +1,12 @@
+#ifndef FIBONACCI_H
+#define FIBONACCI_H
+/**
+ * @file fibonacci.h
+ * @brief Returns fibonacci of a number.
+ *
+ *@param n Number
+ *@return n-th fibonacci number.
+ */
+int fibonacci(int n);
+
+#endif // FIBONACCI_H
